14b8cd0d05f6312f6710fe8f0eddf58145b22fd2
iremtnc <127884544+iremtnc@users.noreply.github.com>
2023-03-14 23:18:39 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+#string=metinsel ifadeleri tutar.
+#integer(int)=tam sayıları göstermekte kullanılır ==> 5,10, -100, 254...
+#double,float=ondalıklı sayıları göstermekte kullanılır ==> 5,3 
+#bool=mantıksal ifadeleri tutar ==>true, false 
+
+#giriş kısmındaki kullanıcı e posta metinsel ifade olduğu için stringdir.
+
+kullaniciadi="iremtnc" 
+sifre=1234 
+print("Kullanıcı adınızı girin:")
+if kullaniciadi!="iremtnc" and sifre!=1234:
+    print("giriş başarısız")
+
+else: 
+    print("giriş başarılı") 
+        
+
+
+
+   
+
+
+
+
+
+
+
+
+
